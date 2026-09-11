745e729349fe8f03fef354a2caaa5bab2c8ac1dc
Lixin Guo <g.lixin@wustl.edu>
2026-09-11 18:17:26 -0500

    Set up the sprite sheet and import the cat sprite, implemented basic four-direction movement

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace player
+{
+    public class UFOController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Move Up - W
+            if (Keyboard.current.wKey.isPressed)
+            {
+                transform.position += new Vector3(0, 0.01f, 0);
+            }
+
+            // Move Down - S
+            if (Keyboard.current.sKey.isPressed)
+            {
+                transform.position += new Vector3(0, -0.01f, 0);
+            }
+
+            // Move Left - A
+            if (Keyboard.current.aKey.isPressed)
+            {
+                transform.position += new Vector3(-0.01f, 0, 0);
+            }
+
+            // Move Right - D
+            if (Keyboard.current.dKey.isPressed)
+            {
+                transform.position += new Vector3(0.01f, 0, 0);
+            }
+        }
+    }
+}
